fb374eff64d7f57511f0e1db23cf4a2688ee6d93
Iris Caffin <iriscaffin@gmail.com>
2016-02-01 17:05:17 -0800

    Finished project no extras

---
+++ 14-Kanban-DataLayer/14-Kanban-DataLayer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14_Kanban_DataLayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new KanbanPart1Entities())
+            {
+                //var List = db.Lists.First();
+                foreach (var List in db.Lists)
+                {
+                    Console.WriteLine(List.Name);
+
+                    foreach (var cards in List.Cards)
+                    {
+                        Console.WriteLine("\t" + cards.Text);
+                    }
+                    Console.WriteLine();
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
